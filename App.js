fd906b6eabaf6ff4e4ec8d62049a215a70f443e1
Adrian Walker <awalker3365@gmail.com>
2021-08-23 23:28:20 -0400

    8-23-21 1128p

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: '',
+            email: "",
+            placeOfBirth: "",
+            phone: "",
+            favoriteFood: ""
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default App
---
+++ Badge.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Component } from "react/cjs/react.production.min";
+
+class Badge extends Component() {
+    constructor() {
+        super()
+        this.state = []
+    }
+    render() {
+        return (
+            <div>
+                <input name="firstName"></input>
+                <input name="lastName"></input>
+            </div>
+        )
+    }
+
+}
+
+
+
+export default Badge
