da8e31c63dd1f9566d767b861f1d40a17cb8427c
Adrian Walker <awalker3365@gmail.com>
2021-09-09 18:19:12 -0400

    9-9-21 619p

--- App.js
+++ App.js
@@ -12,22 +12,63 @@ class App extends Component {
             placeOfBirth: "",
             phone: "",
             favoriteFood: "",
-            about: ""
+            about: "",
+            nextBadge: []
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit() {
+        this.setState(prevState => {
+            return {
+                nextBadge: [...prevState.nextBadge, prevState],
+                firstName: "",
+                lastName: '',
+                email: "",
+                placeOfBirth: "",
+                phone: "",
+                favoriteFood: "",
+                about: ""
+
+            }
+        })
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
-        const newBadges = this.setState.
 
-            return(
-                <div>
-                    <form>
-                        <h1 className="head">Name Badge</h1>
-                        <Badge />
-                        <button>Submit</button>
-                    </form>
-                </div>
-            )
+        return (
+
+            <div>
+                <form className="info" onSubmit={this.handleSubmit}>
+                    <h1 className="head">Name Badge</h1>
+                    <input onChange={this.handleChange} className="text" name="firstName" placeholder="First Name" type="text" min="3"></input>
+                    <input onChange={this.handleChange} className="text" name="lastName" placeholder="Last Name" type="text" min="3"></input>
+
+                    <input onChange={this.handleChange} className="text" name="email" placeholder="E-Mail" type="email" min="3"></input>
+
+                    <input onChange={this.handleChange} className="text" name="placeOfBirth" placeholder="Birthplace" type="text" min="3"></input>
+
+                    <input onChange={this.handleChange} className="tel" name="phone" placeholder="Phone Number" type="tel" min="3"></input>
+
+                    <input onChange={this.handleChange} className="text" name="favoriteFood" placeholder="Favorite Food" type="text" min="3"></input>
+
+                    <input onChange={this.handleChange} id="tellUs" name="about" placeholder="Tell Us About Yourself" type="text" min="3" ></input>
+
+                    <button>Submit</button>
+                    <Badge />
+                </form>
+            </div>
+
+        )
     }
 
 }
--- Badge.js
+++ Badge.js
@@ -1,22 +1,13 @@
 import React from "react"
+// import App from "./App"
 
 
 
 function Badge() {
+
     return (
+
         <div class="info">
-                <input class="text" name="firstName" placeholder="First Name" type="text" min="3"></input>
-                <input class="text" name="lastName" placeholder="Last Name" type="text" min="3"></input>
-
-                <input class="text" name="email" placeholder="E-Mail" type="email" min="3"></input>
-
-                <input class="text" name="placeOfBirth" placeholder="Birthplace" type="text" min="3"></input>
-
-                <input class="tel" name="phone" placeholder="Phone Number" type="tel" min="3"></input>
-
-                <input class="text" name="favoriteFood" placeholder="Favorite Food" type="text" min="3"></input>
-
-                <input id="tellUs" name="about" placeholder="Tell Us About Yourself" type="text" min="3" ></input>
 
         </div>
     )
